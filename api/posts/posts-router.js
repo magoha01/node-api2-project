6855954a77a20265687248279958296ba8c9c3e3
Hailey Magoline <magoline.hailey@gmail.com>
2022-02-08 11:37:40 -0600

    Server up and running

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const router = require('express').Router();
+const Post = require('./posts-model');
+
+//GET /api/posts
+
+//GET /api/posts/:id
+
+//POST /api/posts
+
+//PUT /api/posts
+
+//DELETE /api/posts/:id
+
+//GET /api/posts/:id/comments
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
+const express = require("express");
+
+const server = express();
+
+const postsRouter = require("./posts/posts-router");
+
 // require your posts router and connect it here
+server.use("./api/posts", postsRouter);
+
+module.exports = server;
